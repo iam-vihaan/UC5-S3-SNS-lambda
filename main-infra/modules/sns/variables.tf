be48583f58a5382fc281e9b7e02539718202a21c
iam-vihaan <asathyavihaan@gmail.com>
2025-06-16 17:18:50 +0530

    Update variables.tf

--- main-infra/modules/sns/variables.tf
+++ main-infra/modules/sns/variables.tf
@@ -1,7 +1,17 @@
-variable "topic_name" {}
-variable "tags" {
-  type = map(string)
+# Variables for SNS module
+
+variable "topic_name" {
+  description = "Name of the SNS topic"
+  type        = string
 }
 
+variable "email_subscriptions" {
+  description = "List of email addresses to subscribe to the SNS topic"
+  type        = list(string)
+  default     = []
+}
 
-variable "email" {}
+variable "environment" {
+  description = "Environment (dev, staging, prod)"
+  type        = string
+}
