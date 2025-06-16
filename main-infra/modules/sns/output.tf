e28f73ce557c6ec6a3c06beb85a794becccc5a5f
iam-vihaan <asathyavihaan@gmail.com>
2025-06-16 17:18:35 +0530

    Update output.tf

--- main-infra/modules/sns/output.tf
+++ main-infra/modules/sns/output.tf
@@ -1,3 +1,11 @@
+# Outputs for SNS module
+
+output "topic_name" {
+  description = "Name of the SNS topic"
+  value       = aws_sns_topic.notifications.name
+}
+
 output "topic_arn" {
-  value = aws_sns_topic.topic.arn
+  description = "ARN of the SNS topic"
+  value       = aws_sns_topic.notifications.arn
 }
